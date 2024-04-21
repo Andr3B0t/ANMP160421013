8dcfdf180c1f350780807257a910cf564c66ad16
Andr3B0t <andreasjcool281@gmail.com>
2024-04-21 23:34:15 +0700

    minor visual update

--- app/src/main/java/com/ubaya/anmp160421013/view/LoginFragment.kt
+++ app/src/main/java/com/ubaya/anmp160421013/view/LoginFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
@@ -71,11 +72,13 @@ class LoginFragment : Fragment() {
                         editor.putString("nama_depan", user.nama_depan)
                         editor.putString("nama_belakang", user.nama_belakang)
                         editor.apply()
+                        Toast.makeText(this.requireContext(), "Login Success", Toast.LENGTH_SHORT).show()
                         startActivity(intent)
                         requireActivity().finish()
                     }
                 },
                 {
+                    Toast.makeText(this.requireContext(), "Login Failed", Toast.LENGTH_SHORT).show()
                     Log.e("apiresult", it.message.toString())
                 }) {
                 override fun getParams(): MutableMap<String, String>? {
